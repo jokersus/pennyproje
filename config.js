ecb7ee09c79dd509b7b067e0003c9f5c09a3379e
Bruce Wakefield <bugrasaricam8@gmail.com>
2018-09-07 00:34:54 +0300

    synthax

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-exports.GOOGLE_API_KEY = 'AIzaSyDj7xDagAKs0PmgZIPI6Obg_RW6UfLTlMc';
+
 exports.PREFIX = '>'
--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const { Util } = require('discord.js');
 const fs = require("fs");
-const { GOOGLE_API_KEY, PREFIX } = require('./config.js');
+const { PREFIX } = require('./config.js');
+const token = 'process.env.token';
+const GOOGLE_API_KEY = 'process.env.googletoken';
 const YouTube = require('simple-youtube-api');
 const ytdl = require('ytdl-core');
 const youtube = new YouTube(GOOGLE_API_KEY);
@@ -423,4 +425,4 @@ bot.on("message", async message => {
 
 });
 
-bot.login(botconfig.token);
+bot.login(token).catch(err => console.log(err));
