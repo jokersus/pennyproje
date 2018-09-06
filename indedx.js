e14eb2897933b410d77260861304b491490f6c08
Bruce Wakefield <bugrasaricam8@gmail.com>
2018-09-06 23:59:59 +0300

    synthax

--- indedx.js
+++ indedx.js
@@ -1,28 +1,31 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
+const { Util } = require('discord.js');
 const fs = require("fs");
+const queue = new Map();
 const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection();
-let rcoins = require("./playerequipment/coins.json");
-let coins = require("./coins.json");
-let xp = require("./xp.json");
-let purple = botconfig.purple;
-let cooldown = new Set();
-let cdseconds = 5;
+var rcoins = require("./playerequipment/coins.json");
+var coins = require("./coins.json");
+var xp = require("./xp.json");
+var purple = botconfig.purple;
+var cooldown = new Set();
+var cdseconds = 5;
 const chratis_cooldown_time = 5;
+
 
 fs.readdir("./commands/", (err, files) =>  {
 
   if(err) console.log(err);
 
-  let jsfile = files.filter(f => f.split(".").pop() === "js")
+  var jsfile = files.filter(f => f.split(".").pop() === "js")
   if(jsfile.lenght <= 0){
     console.log("Couldn't find commands.")
     return;
   }
 
   jsfile.forEach((f, i) =>{
-    let props = require(`./commands/${f}`);
+    var props = require(`./commands/${f}`);
     console.log(`${f} loaded!`)
     bot.commands.set(props.help.name, props);
   });
@@ -31,7 +34,7 @@ fs.readdir("./commands/", (err, files) =>  {
 //bot.on("guildMemberAdd", async member => {
   //console.log(`${member.id} joined the server.`);
 
-  //let welcomechannel = member.guild.channels.find(`name`, "smalltalk");
+  //var welcomechannel = member.guild.channels.find(`name`, "smalltalk");
   //welcomechannel.send(`LOOK OUT! ${member} has joined the fam! Make sure to read the #rules and make an #introduction so we can give you roles for gender, location and/or nationality.`);
 //});
 
@@ -39,22 +42,22 @@ fs.readdir("./commands/", (err, files) =>  {
 
   //console.log(`${member.id} left the server.`);
 
-  //let welcomechannel = member.guild.channels.find(`name`, "smalltalk");
+  //var welcomechannel = member.guild.channels.find(`name`, "smalltalk");
   //welcomechannel.send(`BEGONE THOT!`);
 //});
 //${member} has left the server!
 
 bot.on("roleUpdate", async (oldRole, newRole)=> {
 
-  let logchannel = newRole.guild.channels.find(`name`, "log");
+  var logchannel = newRole.guild.channels.find(`name`, "log");
   logchannel.send(`The role ${oldRole.name} has been changed to ${newRole}`);
 
 
 });
 
-bot.on("roleDelete", async role => {
-  let logchannel = message.guild.channels.find(`name`, "smalltalk");
-  logchannel.send(`The role ${role.name} has been deleted.`);
+bot.on("roleDevare", async role => {
+  var logchannel = message.guild.channels.find(`name`, "smalltalk");
+  logchannel.send(`The role ${role.name} has been devared.`);
 });
 
 
@@ -62,24 +65,32 @@ bot.on("channelCreate", async channel => {
 
   console.log(`${channel.name} has been created.`);
 
-  let sChannel = channel.guild.channels.find(`name`, "smalltalk");
+  var sChannel = channel.guild.channels.find(`name`, "smalltalk");
   sChannel.send(`${channel} has been created`);
 
 });
 
-bot.on("channelDelete", async channel => {
+bot.on("channelDevare", async channel => {
 
-  console.log(`${channel.name} has been deleted.`);
+  console.log(`${channel.name} has been devared.`);
 
-  let sChannel = channel.guild.channels.find(`name`, "smalltalk");
-  sChannel.send(`${channel.name} has been deleted`);
+  var sChannel = channel.guild.channels.find(`name`, "smalltalk");
+  sChannel.send(`${channel.name} has been devared`);
 });
 
 
 
 
-
-
+//bot.on('ready', () => {
+    //bot.user.setStatus('available')
+    //bot.user.setPresence({
+        //game: {
+            //name: 'Vloom Streaming',
+            //type: "STREAMING",
+            //url: "https://www.twitch.tv/vloomed"
+        //}
+    //});
+//});
 bot.on("ready", () => {
   console.log(`Penny has started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guilds.`);
   bot.user.setActivity(`with ${bot.users.size} users on INTSL`);
@@ -90,39 +101,39 @@ bot.on("message", async message => {
 
   if(message.author.bot) return;
   if(message.channel.type === "dm") return;
-  let prefix = botconfig.prefix;
+  var prefix = botconfig.prefix;
   if(!message.content.startsWith(prefix)) return;
   if(cooldown.has(message.author.id)){
-    message.delete();
-    let cdembed = new Discord.RichEmbed()
+    message.devare();
+    var cdembed = new Discord.RichEmbed()
     .setAuthor(message.author.username)
     .setColor(botconfig.red)
     .addField("❌Error", "You need to wait 5 secs between commands.");
-    return message.channel.send(cdembed).then(msg => {msg.delete(3000)});1
+    return message.channel.send(cdembed).then(message => {message.devare(3000)});1
   }
   if(!message.member.hasPermission("ADMINISTRATOR")){
     cooldown.add(message.author.id);
   }
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
+  var messageArray = message.content.split(" ");
+  var cmd = messageArray[0];
+  var args = messageArray.slice(1);
 
   if(!rcoins[message.author.id]) coins[message.author.id] = {
     coins: 0
   };
 
-  let chancenum = Math.floor(Math.random()* 15);
-  let onnum = Math.floor(Math.random() * 15);
+  var chancenum = Math.floor(Math.random()* 15);
+  var onnum = Math.floor(Math.random() * 15);
   if(chancenum === onnum){
-    let rcoinamount = chancenum + 1;
+    var rcoinamount = chancenum + 1;
     rcoins[message.author.id] = {
       rcoins: rcoins[message.author.id].coins + rcoinamount
     };
-    let rcoinsembed = new Discord.RichEmbed()
+    var rcoinsembed = new Discord.RichEmbed()
     .setAuthor(message.author.username)
     .setColor(botconfig.purple)
     .addField("💰", `${rcoinamount} pennies added!`);
-    message.channel.send(rcoinsembed).then(msg => msg.delete(5000));
+    message.channel.send(rcoinsembed).then(message => message.devare(5000));
     console.log(`${rcoinamount} pennies added to ${message.author.username}`);
 
     fs.writeFile("./playerequipment/coins.json", JSON.stringify(coins), (err) => {
@@ -131,26 +142,16 @@ bot.on("message", async message => {
 
   }
 
-  let commandfile = bot.commands.get(cmd.slice(prefix.length));
+  var commandfile = bot.commands.get(cmd.slice(prefix.length));
   if(commandfile) commandfile.run(bot,message,args);
 
   setTimeout(() => {
-    cooldown.delete(message.author.id);
+    cooldown.devare(message.author.id);
 }, chratis_cooldown_time * 1000);
 
 });
 
-bot.on("message", async message => {
 
-    if(message.author.bot) return;
-    if(message.channel.type === "dm") return;
-
-    let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
-    if(!prefixes[message.guild.id]){
-      prefixes[message.guild.id] = {
-        prefixes: botconfig.prefix
-      };
-    }
 
     if(!coins[message.author.id]){
       coins[message.author.id] = {
@@ -158,8 +159,8 @@ bot.on("message", async message => {
       };
     }
 
-    let coinAmt = Math.floor(Math.random() * 15) + 1;
-    let baseAmt = Math.floor(Math.random() * 15) + 1;
+    var coinAmt = Math.floor(Math.random() * 15) + 1;
+    var baseAmt = Math.floor(Math.random() * 15) + 1;
     console.log(`${coinAmt} ; ${baseAmt}`);
 
     if(coinAmt === baseAmt){
@@ -169,16 +170,16 @@ bot.on("message", async message => {
     fs.writeFile("./coins.json", JSON.stringify(coins), (err) => {
       if (err) console.log(err)
     });
-    let coinEmbed = new Discord.RichEmbed()
+    var coinEmbed = new Discord.RichEmbed()
     .setAuthor(message.author.username)
     .setColor("#0000FF")
     .addField("<:Pennies_HEADS:470660608736624640>", `${coinAmt} coins added!`);
 
-    //message.channel.send(coinEmbed).then(msg => {msg.delete(5000)});
+    //message.channel.send(coinEmbed).then(message => {message.devare(5000)});
     }
 
 
-    let xpAdd = Math.floor(Math.random() * 7) + 8;
+    var xpAdd = Math.floor(Math.random() * 7) + 8;
   console.log(xpAdd);
 
   if(!xp[message.author.id]){
@@ -188,34 +189,33 @@ bot.on("message", async message => {
     };
   }
 
-  let curxp = xp[message.author.id].xp;
-  let curlvl = xp[message.author.id].level;
-  let nxtLvl = xp[message.author.id].level * 800;
+  var curxp = xp[message.author.id].xp;
+  var curlvl = xp[message.author.id].level;
+  var nxtLvl = xp[message.author.id].level * 800;
   xp[message.author.id].xp =  curxp + xpAdd;
   if(nxtLvl <= xp[message.author.id].xp){
     xp[message.author.id].level = curlvl + 1;
-    let lvlup = new Discord.RichEmbed()
+    var lvlup = new Discord.RichEmbed()
     .setAuthor(message.author.username)
     .setTitle("Level Up!")
     .setColor(purple)
     .addField("New Level", curlvl + 1);
 
 
-    //message.channel.send(lvlup).then(msg => {msg.delete(5000)});
+    //message.channel.send(lvlup).then(message => {message.devare(5000)});
   }
   fs.writeFile("./xp.json", JSON.stringify(xp), (err) => {
     if(err) console.log(err)
   });
 
 
-    let prefix = prefixes[message.guild.id].prefixes;
-    let messageArray = message.content.split(" ");
-    let cmd = messageArray[0];
-    let args = messageArray.slice(1);
+    var prefix = prefixes[message.guild.id].prefixes;
+    var messageArray = message.content.split(" ");
+    var cmd = messageArray[0];
+    var args = messageArray.slice(1);
 
-    let commandfile = bot.commands.get(cmd.slice(prefix.length));
+    var commandfile = bot.commands.get(cmd.slice(prefix.length));
     if(commandfile) commandfile.run(bot,message,args);
-
 
 
 });
