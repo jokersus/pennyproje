77e6995cf9cee3af2ec4438f65eb686117cacc22
arespectra <43042472+arespectra@users.noreply.github.com>
2018-09-10 19:46:42 +0300

    permissions change

--- commands/prefix.js
+++ commands/prefix.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 
 module.exports.run = async (bot, message, args) => {
 
-  if(!message.member.hasPermission("MANAGE_SERVER")) return message.reply("Don't think so, buddy.");
+  if(!message.member.hasPermission("ADMIN")) return message.reply("Don't think so, buddy.");
   if(!args[0] || args[0 == "help"]) return message.reply("Usage p>prefix <desired prefix here>");
 
   let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
